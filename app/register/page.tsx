eaac3a3b207f27b5eadcc0f9c69c4491c5576e72
rkaa <mp8359620@gmail.com>
2026-01-24 08:27:53 +0700

    warningTest

---
+++ app/register/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+
+export default function Register() {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[nama, setNama] = useState("");
+    const[showWarn, setShowWarn] = useState<{message: string; type: 'error' | 'success';} | null>(null)
+
+
+    const Submit = async(e: React.FormEvent<HTMLFormElement>)  => {
+        e.preventDefault();
+
+        if(!email || !password) {
+            setShowWarn(message: "Semua Field wajib diisi!", type: 'error')
+            return;
+        }
+        const {data, error} = await supabase.auth.signUp({
+            email: email,
+            password: password,
+            options: {
+                data: {display_name: nama,
+                },
+            },
+        });
+
+        if(error) {
+            setShowWarn(error.message);
+        }  else {
+            alert("User Berhasil dimasukkan")
+        }
+        setNama("");
+        setEmail("");
+        setPassword("");
+    }
+    return(
+        <form onSubmit={Submit} className="flex flex-col gap-2 w-64">
+            <label>Username</label>
+            <input
+            type="text"
+            value={nama}
+            onChange={(e) => setNama(e.target.value)}
+            className="border p-2 rounded"
+            />
+            <label>Email</label>
+            <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+
+            className="border p-2 rounded"
+            />
+
+            <label>Password</label>
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+
+            className="border p-2 rounded"
+            />
+            {showWarn && (
+                <div className="text-red-600">
+                    {showWarn}
+                </div>
+            )}
+
+            <button type="submit" className="bg-purple-600 text-white p-2 rounded">
+            Register
+            </button>
+      </form>
+    )
+}
